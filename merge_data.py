7cf8a637c18b9dd803e516df7aff0ada2288d2aa
Srihariharan <137597513+Hariharan2134@users.noreply.github.com>
2025-07-15 16:07:41 +0530

    Update and rename merge_annotations_physio.py to merge_data.py

--- merge_data.py
+++ merge_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
+# Define file paths
 annotations_path = '/content/drive/My Drive/annotations/sub111.csv'
 physiological_path = '/content/drive/My Drive/physiological/sub_2.csv'
-merged_csv_path = '/content/merged_data.csv'
 
+merged_csv_path = '/content/merged_data.csv'
 chunk_size = 5000
+
 merged_df = pd.DataFrame()
 
 annotations_iter = pd.read_csv(annotations_path, chunksize=chunk_size)
@@ -14,7 +16,8 @@ for annotations_chunk, physiological_chunk in zip(annotations_iter, physiologica
     chunk_merge = pd.merge(
         physiological_chunk[['bvp', 'gsr', 'video']],
         annotations_chunk[['valence', 'arousal', 'video']],
-        on='video', how='inner'
+        on='video',
+        how='inner'
     )
     merged_df = pd.concat([merged_df, chunk_merge], ignore_index=True)
 
